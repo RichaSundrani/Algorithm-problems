275a2b3c0faebab8360e7503d471d01488adaa67
RichaSundrani <sundrani1177@gmail.com>
2021-04-04 13:50:28 -0500

    update

--- Trees and Graphs/path_max_min_sum.js
+++ Trees and Graphs/path_max_min_sum.js
@@ -1,7 +1,7 @@
 /*
-Given a matrix of integers A with R rows and C columns, find the maximum score of a path starting at [0,0] and ending at [R-1,C-1].
+Given a matrix of integers matrixwith R rows and C columns, find the maximum score of a path starting at [0,0] and ending at [R-1,C-1].
 The score of a path is the minimum value in that path.  For example, the value of the path 8 →  4 →  5 →  9 is 4.
-A path moves some number of times from one visited cell to any neighbouring unvisited cell in one of the 4 cardinal directions 
+matrixpath moves some number of times from one visited cell to any neighbouring unvisited cell in one of the 4 cardinal directions 
 (north, east, west, south).
 
 Example 1:
@@ -18,37 +18,82 @@ Output: 3
 
 Note:
 1 <= R, C <= 100
-0 <= A[i][j] <= 10^9
+0 <= matrix[i][j] <= 10^9
 */
 
 /**
- * @param {number[][]} A
+ * @param {number[][]} matrix
  * @return {number}
  */
- function maximumMinimumPath(A) {
-    if (A == null || A.length === 0 || A[0].length === 0) return 0;
-    const h = A.length;
-    const w = A[0].length;
-    const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
-    const q = [[0, 0]];
-    const dp = [...Array(h)].map(() => Array(w).fill(0));
-    dp[0][0] = A[0][0];
-  
-    while (q.length) {
-      const [x, y] = q.shift();
-      for (const [dx, dy] of dirs) {
-        const i = x + dx;
-        const j = y + dy;
-        if (i < 0 || j < 0 || i >= h || j >= w) continue;
-        const score = Math.min(A[i][j], dp[x][y]);
-        if (score > dp[i][j]) {
-          // only continue if it is on the the path that has bigger score
-          dp[i][j] = score;
-          q.push([i, j]);
-        }
-      }
-    }
-  
-    return dp[h - 1][w - 1];
+
+var maximumMinimumPath = function (matrix) {
+  let row = matrix.length;
+  if (row === 0) {
+    return -1;
   }
-console.log(maximumMinimumPath([[5,4,5],[1,2,6],[7,4,6]]))
+  let col = matrix[0].length;
+  let dp = Array.from({ length: row }, () => []);
+  console.log(dp);
+  dp[0][0] = matrix[0][0];
+  let queue = [[0, 0]];
+  let directions = [[-1, 0], [0, -1], [1, 0], [0, 1]];
+  while (queue.length > 0) {
+    let len = queue.length;
+    console.log(len);
+    let newQueue = [];
+    for (let i = 0; i < len; i++) {
+      let [x, y] = queue.pop();
+      directions.forEach(([dx, dy]) => {
+        let tx = x + dx;
+        let ty = y + dy;
+        if (tx < 0 || ty < 0 || tx >= row || ty >= col) return;
+        let newVal = Math.min(matrix[tx][ty], ~~dp[x][y]);
+        if (newVal > ~~dp[tx][ty]) {
+          // valid path, updated
+          dp[tx][ty] = newVal;
+          newQueue.push([tx, ty]);
+        }
+      })
+    }
+    queue = newQueue;
+  }
+
+  return ~~dp[row - 1][col - 1];
+};
+
+console.log(maximumMinimumPath([[5, 4, 5], [1, 2, 6], [7, 4, 6]]));
+
+
+
+
+
+
+
+//  function maximumMinimumPath(matrix) {
+//     if (matrix == null || matrix.length === 0 || matrix[0].length === 0) {
+//       return 0;
+//     }
+//     const height = matrix.length;
+//     const width = matrix[0].length;
+//     const directions = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+//     const queue = [[0, 0]];
+//     const dp = [...Array(height)].map(() => Array(width).fill(0));
+//     dp[0][0] = matrix[0][0];
+
+//     while (queue.length) {
+//       const [x, y] = queue.shift();
+//       for (const [dx, dy] of directions) {
+//         const i = x + dx;
+//         const j = y + dy;
+//         if (i < 0 || j < 0 || i >= height || j >= width) continue;
+//         const score = Math.min(matrix[i][j], dp[x][y]);
+//         if (score > dp[i][j]) {
+//           // only continue if it is on the the path that has bigger score
+//           dp[i][j] = score;
+//           queue.push([i, j]);
+//         }
+//       }
+//     }
+
+//     return dp[height - 1][width - 1];
+//   }
